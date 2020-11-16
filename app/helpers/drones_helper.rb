6c335df2253c9ae632614bb13c03ee46b7204483
leticia <letisaless@gmail.com>
2020-11-16 18:35:58 -0300

    add test helper and helper

--- app/helpers/drones_helper.rb
+++ app/helpers/drones_helper.rb
@@ -1,37 +1,59 @@
 module DronesHelper
 
-  def result(action)
+  def position(action)
     x = 0
     y = 0
     chars = action.chars
-    action = ''
+    move = ''
     count = ''
 
     chars.each_with_index do |char, idx|
+      puts("\n\n")
+      puts("char: " + char)
+
       if ["N", "S", "L", "O"].include? char
-        substring = chars.from(idx)
-        step = 0
-
-        while !["N", "S", "L", "O"].include? substring[step]
-          count = count + substring[step]
-          step = step + 1
-        end
-
-        puts(count)
-
-        case char
+        case move
           when "N"
-            y = y + 1
+            y = y + count.to_i
           when "S"
-            y = y - 1
+            y = y - count.to_i
           when "L"
-            x = x + 1
+            x = x + count.to_i
           when "O"
-            x = x - 1
+            x = x - count.to_i
         end
-      end
-    end
 
-    "(#{x}, #{y})"
-  end 
+        move = char
+        substring = action[idx + 1..0]
+        step = 0
+        count = ''
+
+        while !["N", "S", "L", "O", "X"].include? substring[step]
+          if substring[step].to_i.to_s == substring[step]
+            count = count + substring[step]
+            step = step + 1
+          else
+            return "(999, 999)"
+          end
+        end
+
+        count = count.to_i > 0 ? count.to_i : 1
+      end
+
+      puts "(#{x}, #{y})"
+
+      case action[-1]
+        when "N"
+          y = y + count.to_i
+        when "S"
+          y = y - count.to_i
+        when "L"
+          x = x + count.to_i
+        when "O"
+          x = x - count.to_i
+      end
+
+      return "(#{x}, #{y})"
+    end
+  end
 end
--- test/helpers/drone_helper_test.rb
+++ test/helpers/drone_helper_test.rb
@@ -2,12 +2,32 @@ require 'test_helper'
 include DronesHelper
 
 class DronesHelperTest < ActiveSupport::TestCase
+   tests DronesHelper
+
   # test "the truth" do
   #   assert true
   # end
 
   test 'calculates final drone position' do
     assert_equal "(5, 5)",
-                 result("NNNNNLLLLL")
+                 result(moves(:one).action)
+                 
+    assert_equal "(5, 5)",
+                 result(moves(:two).action)
+                 
+    assert_equal "(4, 4)",
+                 result(moves(:three).action)
+                 
+    assert_equal "(-5, -5)",
+                 result(moves(:four).action)
+                 
+    assert_equal "(21, 21)",
+                 result(moves(:eleven).action)
+                 
+    assert_equal "(999, 999)",
+                 result(moves(:twelve).action)
+
+    assert_equal "(0, 1)",
+                 result(moves(:eighteen).action)
   end
 end
