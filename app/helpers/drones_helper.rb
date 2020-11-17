5c84890bcf4a26f759a9945db44b708b99753dca
leticia <letisaless@gmail.com>
2020-11-17 13:28:45 -0300

    add to stack before running

--- app/helpers/drones_helper.rb
+++ app/helpers/drones_helper.rb
@@ -6,23 +6,14 @@ module DronesHelper
     chars = action.chars
     move = ''
     count = ''
+    stack_moves = []
+    stack_count = []
 
     chars.each_with_index do |char, idx|
-      if char == "X"
-        move = ""
-        count = ""
-      elsif ["N", "S", "L", "O"].include? char
-        case move
-          when "N"
-            y = y + count.to_i
-          when "S"
-            y = y - count.to_i
-          when "L"
-            x = x + count.to_i
-          when "O"
-            x = x - count.to_i
-        end
-
+      if char == "X" # cancel last action
+        stack_moves.pop
+        stack_count.pop
+      elsif ["N", "S", "L", "O"].include? char # create action
         move = char
         substring = action.from(idx + 1)
         step = 0
@@ -37,24 +28,43 @@ module DronesHelper
           end
         end
 
-        puts "count: " + count
         count = count.empty? ? 1 : count.to_i
-      elsif char != char.to_i.to_s    
+
+        stack_moves.push(char)
+        stack_count.push(count)
+      elsif char == char.to_i.to_s
+        if idx == 0 # if starts with number
+          return "(999, 999)"
+        elsif 
+          step = idx
+          move = action[idx]
+          while step >= 0 and move.to_i.to_s == move # search for previous move
+            step = step - 1
+            move = action[step]
+          end
+          if move == "X" # X shouldn't allow step
+            return "(999, 999)"
+          end
+        end
+      else
         return "(999, 999)"
       end
     end
 
-    puts "(#{x}, #{y})"
-
-    case move
-      when "N"
-        y = y + count.to_i
-      when "S"
-        y = y - count.to_i
-      when "L"
-        x = x + count.to_i
-      when "O"
-        x = x - count.to_i
+    stack_moves.each_with_index do |move, idx|
+      if stack_count[idx] > 2147483647 
+        return "(999, 999)"
+      end
+      case move
+        when "N"
+          y = y + stack_count[idx]
+        when "S"
+          y = y - stack_count[idx]
+        when "L"
+          x = x + stack_count[idx]
+        when "O"
+          x = x - stack_count[idx]
+      end
     end
 
     return "(#{x}, #{y})"
