03ebc20fd6778772cbd75f7274f31722f79b05a8
leticia <letisaless@gmail.com>
2020-11-17 12:26:41 -0300

    add all tests

--- test/helpers/drone_helper_test.rb
+++ test/helpers/drone_helper_test.rb
@@ -26,8 +26,73 @@ class DronesHelperTest < ActiveSupport::TestCase
                  position(moves(:four).action)
   end
 
+  test 'calculates final drone position with move five' do
+    assert_equal "(-5, -5)",
+                 position(moves(:five).action)
+  end
+
+  test 'calculates final drone position with move six' do
+    assert_equal "(999, 999)",
+                 position(moves(:six).action)
+  end
+
+  test 'calculates final drone position with move seven' do
+    assert_equal "(1, 123)",
+                 position(moves(:seven).action)
+  end
+
+  test 'calculates final drone position with move eight' do
+    assert_equal "(1, 1)",
+                 position(moves(:eight).action)
+  end
+
+  test 'calculates final drone position with move nine' do
+    assert_equal "(1, 2)",
+                 position(moves(:nine).action)
+  end
+
+  test 'calculates final drone position with move ten' do
+    assert_equal "(21, 21)",
+                 position(moves(:ten).action)
+  end
+
+  test 'calculates final drone position with move eleven' do
+    assert_equal "(21, 21)",
+                 position(moves(:eleven).action)
+  end
+
   test 'calculates final drone position with move twelve (error)' do
     assert_equal "(999, 999)",
                  position(moves(:twelve).action)
+  end
+
+  test 'calculates final drone position with move thirteen (error)' do
+    assert_equal "(999, 999)",
+                 position(moves(:thirteen).action)
+  end
+
+  test 'calculates final drone position with move fourteen (error)' do
+    assert_equal "(999, 999)",
+                 position(moves(:fourteen).action)
+  end
+
+  test 'calculates final drone position with move fifteen (error)' do
+    assert_equal "(999, 999)",
+                 position(moves(:fifteen).action)
+  end
+
+  test 'calculates final drone position with move sixteen (error)' do
+    assert_equal "(999, 999)",
+                 position(moves(:sixteen).action)
+  end
+
+  test 'calculates final drone position with move seventeen (error)' do
+    assert_equal "(999, 999)",
+                 position(moves(:seventeen).action)
+  end
+
+  test 'calculates final drone position with move eighteen (error)' do
+    assert_equal "(0, 1)",
+                 position(moves(:eighteen).action)
   end
 end
